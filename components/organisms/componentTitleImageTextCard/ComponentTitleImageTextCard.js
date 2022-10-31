8d494ab75169d672bb00214327793fae0622790a
Kirk Daniels <kirkd@ayoba.me>
2022-10-31 09:15:18 +0200

    carousel

--- components/organisms/componentTitleImageTextCard/ComponentTitleImageTextCard.js
+++ components/organisms/componentTitleImageTextCard/ComponentTitleImageTextCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import classes from "./ComponentTitleImageTextCard.module.scss";
 
