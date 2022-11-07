4e83e1436c4f4ddde7e67a7f7315c9d38008bb74
Kirk Daniels <kirkd@ayoba.me>
2022-11-07 13:31:38 +0200

    legal info sttyling

--- components/blocks/componentFooter/ComponentFooter.js
+++ components/blocks/componentFooter/ComponentFooter.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import classes from "./ComponentFooter.module.scss";
 
 function Footer({ contentModule }) {
-  const { linkList } = contentModule;
-  console.log("Links", linkList);
+  const { image, linkList } = contentModule;
+  console.log("Footer", contentModule);
   return (
     <footer className={classes.oFooter}>
       <div className={`container`}>
         <div className={`row`}>
-          <div className={`col-12 col-md-3 footer-col-1`}>hello</div>
-          <div className={`col-12 col-md-3 footer-col-2`}>hello</div>
+          <div className={`col-12 col-md-3 footer-col-1`}>
+            <figure className={classes.mLogo}>
+              <Image
+                className={`${classes.aImage} a-responsive-image`}
+                src={image?.fields?.file?.url}
+                alt={`title`}
+                width={image.fields.file.details.image.width}
+                height={image.fields.file.details.image.height}
+                aria-hidden="true"
+                layout="responsive"
+                priority="true"
+              />
+            </figure>
+          </div>
+          <div className={`col-12 col-md-3 footer-col-2`}></div>
           <nav
             className={`col-12 col-md-5 offset-md-1 col-lg-4 offset-lg-2 footer-col-3`}
           >
@@ -18,7 +32,10 @@ function Footer({ contentModule }) {
               {linkList.map((item, index) => (
                 <li key={index}>
                   <Link href={`${item.fields.url}`}>
-                    <a className={`aBtn a-fnt-16s btnAlt`} rel="noopener">
+                    <a
+                      className={`${classes.aLink} aBtn a-fnt-16s`}
+                      rel="noopener"
+                    >
                       {item.fields.text}
                     </a>
                   </Link>
