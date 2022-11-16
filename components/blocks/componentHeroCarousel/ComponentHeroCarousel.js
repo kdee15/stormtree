788c7fcb6f23118e14963257138b4a7bb6fbfe1c
Kirk Daniels <kirkdaniels@me.com>
2022-11-16 16:02:04 +0200

    logo update

--- components/blocks/componentHeroCarousel/ComponentHeroCarousel.js
+++ components/blocks/componentHeroCarousel/ComponentHeroCarousel.js
@@ -2,9 +2,10 @@ import Slider from "react-slick";
 import classes from "./ComponentHeroCarousel.module.scss";
 import Image from "next/image";
 import ComponentCarouselImage from "../../molecules/componentCarouselImage/ComponentCarouselImage";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 function ComponentHeroCarousel({ contentModule }) {
-  const { logo, carouselImages } = contentModule;
+  const { logo, description, carouselImages } = contentModule;
 
   const settings = {
     dots: true,
@@ -19,18 +20,24 @@ function ComponentHeroCarousel({ contentModule }) {
 
   return (
     <section className={classes.oHeroCarousel}>
-      <figure className={classes.mLogo}>
-        <Image
-          className={`${classes.aImage} a-responsive-image`}
-          src={logo.fields.file.url}
-          alt={`title`}
-          width={logo.fields.file.details.image.width}
-          height={logo.fields.file.details.image.height}
-          aria-hidden="true"
-          layout="responsive"
-          priority="true"
-        />
-      </figure>
+      <div className={classes.oContent}>
+        <figure className={classes.mLogo}>
+          <Image
+            className={`${classes.aImage} a-responsive-image`}
+            src={logo.fields.file.url}
+            alt={`title`}
+            width={logo.fields.file.details.image.width}
+            height={logo.fields.file.details.image.height}
+            aria-hidden="true"
+            layout="responsive"
+            priority="true"
+          />
+        </figure>
+
+        <div className={`${classes.aSubText} fnt72`}>
+          {documentToReactComponents(description)}
+        </div>
+      </div>
       <div className={`${classes.oCarouselBlock} oCarouselBlock`}>
         <Slider {...settings}>
           {carouselImages.map((item) => (
